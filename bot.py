78d225e15c706d8539661c1e087305f13b0d01d8
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-02 12:43:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ api_hash = 'api hash'
 token = "token"
 
 
-Client = Client("messageviewcounterbot", api_id, api_hash, bot_token=token)
+app = Client(':memory:', api_id, api_hash, bot_token=token)
 
 
 forwardchannel = channel id
@@ -16,15 +16,15 @@ text
 """
 
 
-@Client.on_message(filters.command("start") & filters.private)
+@app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
    await message.reply(startmsg,
 )
 
-@Client.on_message(~filters.service & ~filters.game & ~filters.channel & ~filters.edited & ~filters.linked_channel)
+@app.on_message(~filters.service & ~filters.game & ~filters.channel & ~filters.edited & ~filters.linked_channel)
 async def viewcounter(client, message):
     forward = await message.forward(forwardchannel)
     await forward.forward(message.chat.id)
     
     
-Client.run()
+app.run()
