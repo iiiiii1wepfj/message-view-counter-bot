8e108d6a57feb813cbcecb8fae455255b50535ce
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-03-23 14:13:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ token = "token"
 
 app = Client(':memory:', api_id, api_hash, bot_token=token)
 
-anonymous_poll = filters.create(lambda *_: _[2].poll is not None and not _[2].poll.is_anonymous)
+non_anonymous_poll = filters.create(lambda *_: _[2].poll is not None and not _[2].poll.is_anonymous)
 
 forwardchannel = -1000000000000
 startmsg  = """
@@ -22,12 +22,12 @@ async def start(client, message):
    await message.reply(startmsg,
 )
 
-@app.on_message(~filters.service & ~filters.game & ~filters.channel & ~filters.edited & ~filters.linked_channel & ~anonymous_poll)
+@app.on_message(~filters.service & ~filters.game & ~filters.channel & ~filters.edited & ~filters.linked_channel & ~non_anonymous_poll)
 async def viewcounter(client, message):
     forward = await message.forward(forwardchannel)
     await forward.forward(message.chat.id)
     
-@app.on_message((filters.service | filters.game | filters.channel | anonymous_poll) & ~filters.edited)
+@app.on_message((filters.service | filters.game | filters.channel | non_anonymous_poll) & ~filters.edited)
 async def notsupported(client, message):
  await message.reply("sorry but this type of message not supported (non anonymous polls or games (like @gamebot or @gamee) or message from channels or service messages)", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("delete this message", "deleterrormessage")]]))
 
