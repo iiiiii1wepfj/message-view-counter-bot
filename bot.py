3b92d34a95f8b4a23b0899d7984980ad5a063b92
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-07-10 09:50:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,11 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, Poll, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import (
+    Message,
+    Poll,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    CallbackQuery,
+)
 
 
 api_id = api id
@@ -7,33 +13,55 @@ api_hash = 'api hash'
 token = "token"
 
 
-app = Client(':memory:', api_id, api_hash, bot_token=token)
+app = Client(":memory:", api_id, api_hash, bot_token=token)
 
-non_anonymous_poll = filters.create(lambda *_: _[2].poll is not None and not _[2].poll.is_anonymous)
+non_anonymous_poll = filters.create(
+    lambda *_: _[2].poll is not None and not _[2].poll.is_anonymous
+)
 
 forwardchannel = -1000000000000
-startmsg  = """
+startmsg = """
 text
 """
 
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-   await message.reply(startmsg,
-)
+    await message.reply(
+        startmsg,
+    )
 
-@app.on_message(~filters.service & ~filters.game & ~filters.channel & ~filters.edited & ~filters.linked_channel & ~non_anonymous_poll)
+
+@app.on_message(
+    ~filters.service
+    & ~filters.game
+    & ~filters.channel
+    & ~filters.edited
+    & ~filters.linked_channel
+    & ~non_anonymous_poll
+)
 async def viewcounter(client, message):
     forward = await message.forward(forwardchannel)
     await forward.forward(message.chat.id)
-    
-@app.on_message((filters.service | filters.game | filters.channel | non_anonymous_poll) & ~filters.edited)
-async def notsupported(client, message):
- await message.reply("sorry but this type of message not supported (non anonymous polls or games (like @gamebot or @gamee) or message from channels or service messages)", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("delete this message", "deleterrormessage")]]))
+    await forward.delete()
 
-    
+
+@app.on_message(
+    (filters.service | filters.game | filters.channel | non_anonymous_poll)
+    & ~filters.edited
+)
+async def notsupported(client, message):
+    await message.reply(
+        "sorry but this type of message not supported (non anonymous polls or games (like @gamebot or @gamee) or message from channels or service messages)",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("delete this message", "deleterrormessage")]]
+        ),
+    )
+
+
 @app.on_callback_query(filters.regex("^deleterrormessage"))
 async def delerrmsg(client: app, cquery: CallbackQuery):
     await cquery.message.delete()
-    
+
+
 app.run()
