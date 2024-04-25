ffe4a892f548ea2249301a41f0d04dc0007135ba
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2024-04-25 13:46:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-from pyrogram import Client, filters
-from pyrogram.types import (
+from hydrogram import Client, filters
+from hydrogram.types import (
     Message,
     Poll,
     InlineKeyboardMarkup,
